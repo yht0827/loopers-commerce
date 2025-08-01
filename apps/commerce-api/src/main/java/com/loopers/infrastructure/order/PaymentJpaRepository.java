a735566c7617e761afe265139fb901d8cf401d73
yht0827 <yangheetae0827@naver.com>
2025-08-01 13:19:34 +0900

    :sparkles: 코드 리펙토링

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/order/PaymentJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.loopers.infrastructure.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.loopers.domain.order.Payment;
-
-public interface PaymentJpaRepository extends JpaRepository<Payment, Long> {
-}
