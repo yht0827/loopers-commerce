06b051e681beaa6a0cca91f2c51f0dac871d17db
yht0827 <yangheetae0827@naver.com>
2025-09-19 11:20:11 +0900

    :recycle: refactor: 코드 정리

---
+++ apps/commerce-streamer/src/main/java/com/loopers/batch/job/ProductScoreRow.java
@@ -0,0 +1,5 @@
+package com.loopers.batch.job;
+
+public record ProductScoreRow(Long productId, Double score) {
+}
+
--- apps/commerce-streamer/src/main/java/com/loopers/domain/metrics/ProductScoreRow.java
+++
@@ -1,5 +0,0 @@
-package com.loopers.domain.metrics;
-
-public record ProductScoreRow(long productId, double score) {
-}
-
